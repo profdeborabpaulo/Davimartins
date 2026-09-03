0c31b330ea3405b374c48bfbce2e071651e118f8
∫vikaidk dx <davimartinslp2009@gmail.com>
2026-09-03 08:35:59 -0300

    versão 3 do projeto AppTarefas
    atualização do arquivo indext.tsx e envio do arquivo orientações

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,49 +1,68 @@
- import { View, Text, TextInput, Stylesheet } from 'react-native';
-import { useState } from 'react';
-
-export default function HomeScreen() {
+//Importando os componentes do react native
+import { View, Text, TextInput, StyleSheet} from 'react-native';
+import { useState} from 'react';
+ 
+export default function HomeScreen(){
   const[tarefa, setTarefa]=useState('');
-  const[tarefa, setTarefa]=useState([
-    'estudar react-native',
-    'aprender useState',
-    'criar a primeira tela',
+  const[tarefas, setTarefas]=useState([
+    'Estudar react native',
+    'Aprender useState',
+    'Criar a primeira tela',
   ]);
-  
-  return (
-    <Stack>
-      <Stack.Screen
-        name="index"
-        options={{
-          title: "Agendador de Tarefas",
-        }}
+ 
+ 
+//Criar a função de chamada adicionarTarefa
+ 
+  function adicionarTarefa() {
+    if(!tarefa.trim())// impede tarefas em branco
+    return;
+    setTarefas([...tarefas, tarefa]);
+    setTarefa('');
+  }
+ 
+  //Próxima etapa:
+ 
+  return(
+    <View style={styles.container}>
+      <Text style={styles.title}>Gerenciador de Tarefas</Text>
+ 
+      <TextInput
+      placeholder='Digite uma tarefa:'
+      value={tarefa}
+      onChangeText={setTarefa}
       />
 
-      <Stack.Screen
-        name="nova-tarefa"
-        options={{
-          title: "Nova Tarefa",
-        }}
-      />
-    </Stack>
+      {tarefas.map((item, index)=>(
+      <Text
+      key={index}
+    style={styles.item}
+  >
+    .{item}
+    </Text>
+  ))}
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
+ 
+//Criando o estilo para o index.tsx
+const styles=StyleSheet.create({
+  container:{
     flex: 1,
     padding: 20,
+ 
   },
-
-  title: {
+  title:{
     fontSize: 24,
-    fontWeight: "bold",
+    fontWeight: 'bold',
     marginBottom: 16,
   },
-
-  input: {
+  input:{
     borderWidth: 1,
-    borderColor: "#ccc",
+    borderColor: '#ccc',
     borderRadius: 8,
     padding: 12,
   },
+  item
+ 
 });
+ 
